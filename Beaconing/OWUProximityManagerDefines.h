02ae62ba6a283aa2e5a506e77e27301a29f81b95
David Ohayon <david@evertrue.com>
2013-10-12 03:13:05 -0400

    reorg for cocoapods

--- Beaconing/OWUProximityManagerDefines.h
+++ Beaconing/OWUProximityManagerDefines.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2013 ohwutup software. All rights reserved.
 //
 
-static NSString * const kOWUBeaconRegionID = @"OWUBeaconRegion";
-static NSString * const kOWUBeaconProximityUUID = @"CA1D8966-8C45-4E4D-B79B-47FB9677F7E4";
-static NSString * const kOWUBluetoothServiceUUID = @"1736F87F-96E4-464C-A808-4AC508ECFFA2";
-static NSString * const kOWUBluetoothCharacteristicUUID = @"C3ED21D3-A8D5-471B-A375-9D6313A151EF";
+static NSString * const kOWUBeaconRegionID = @"";
+static NSString * const kOWUBeaconProximityUUID = @"";
+static NSString * const kOWUBluetoothServiceUUID = @"";
+static NSString * const kOWUBluetoothCharacteristicUUID = @"";
